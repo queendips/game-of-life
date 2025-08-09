aa5122d1768fe73482497bca0cdcb575612f55f5
queendips <dipali26naygaonkar@gmail.com>
2025-08-09 15:06:59 +0000

    Add dummy test for Jenkins reporting

---
+++ gameoflife-core/src/test/java/com/example/DummyTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class DummyTest {
+    @Test
+    public void testAlwaysPasses() {
+        assertTrue(true);
+    }
+}
